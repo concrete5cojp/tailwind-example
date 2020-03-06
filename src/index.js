697b4e8089f580e0b6c270aca316da4be53d9563
Derek Cameron <derek@concrete5.co.jp>
2020-03-06 17:08:31 +0900

    update to use public path assets for images/css add babel for imports

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('hello-world')
+
+import uniqs from 'uniqs'
+
+console.log(uniqs(1,1,2,1,2,12,1));
--- webpack.config.js
+++ webpack.config.js
@@ -11,86 +11,88 @@ module.exports = {
         ],
         },
     module: {
-        rules: [
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/ig,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.html$/i,
+          use: [
+            'file-loader?name=[name].[ext]', 
+            'extract-loader',
             {
-                test: /\.(png|jpe?g|gif|svg|webp)$/i,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                        name: "[name].[ext]",
-                        outputPath: 'assets/images',
-                        publicPath: '/assets/images',
-                        esModule: false,
-                    }
-                  },
-                ],
-              },
-              {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    
-                        {
-                            loader: "file-loader",
-                            options: {
-                              name: "[name].css",
-                              outputPath: 'assets/css',
-                              publicPath: '/assets/css',
-                              esModule: false,
-                          },
-                        },
-                        "extract-loader",
-                  // Translates CSS into CommonJS
-                  'css-loader',
-                  'postcss-loader',
-                  // Compiles Sass to CSS
-                  {
-                    loader: 'sass-loader',
-                    options: {
-                    // Prefer `dart-sass`
-                    implementation: require('sass'),
-                  },
-                 }
-                ],
-              },
-              {
-                test: /\.css$/i,
-                use: [
-                 /*   {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: (resourcePath, context) => {
-                                return path.relative(path.dirname(resourcePath), context) + '/';
-                                },
-                            hmr: devMode,
-                            minimize: { discardComments: { removeAll: !devMode} }
-                        },
-                      },*/
-                      {
-                        loader: "file-loader",
-                        options: {
-                          name: "[name].css",
-                          outputPath: 'assets/css',
-                          publicPath: '/assets/css',
-                          esModule: false,
-                      },
-                    },
-                    "extract-loader",
-                  // Translates CSS into CommonJS
-                  'css-loader',
-                  'postcss-loader',
-                ],
-              },
+              loader:'html-loader', 
+              options: { 
+                attrs: ['link:href',':srcset','img:data-src', 'img:src', 'audio:src', 'video:src', 'track:src', 'embed:src', 'source:src', 'input:src', 'object:data']
+              }
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|webp)$/i,
+          use: [
             {
-                test: /\.html$/i,
-                use: ['file-loader?name=[name].[ext]', 'extract-loader', 
-                
-                {loader:'html-loader', options: { attrs: ['link:href',':srcset','img:data-src', 'img:src', 'audio:src', 'video:src', 'track:src', 'embed:src', 'source:src', 'input:src', 'object:data']}}],
-                
+              loader: 'file-loader',
+              options: {
+                  name: '[name].[ext]',
+                  outputPath: 'assets/images',
+                  publicPath: '/assets/images',
+                  esModule: false,
+              }
+            },
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                  name: '[name].css',
+                  outputPath: 'assets/css',
+                  publicPath: '/assets/css',
+                  esModule: false,
               },
-          
-        ],
-      },
+            },
+            'extract-loader',
+            'css-loader',
+            'postcss-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                // Prefer `dart-sass`
+                implementation: require('sass'),
+              },
+            }
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].css',
+                outputPath: 'assets/css',
+                publicPath: '/assets/css',
+                  esModule: false,
+              },
+            },
+            'extract-loader',
+            'css-loader',
+            'postcss-loader',
+          ],
+        },
+        
+      ],
+    },
     devServer: {
         contentBase: path.join(__dirname, 'src'),
         compress: true,
